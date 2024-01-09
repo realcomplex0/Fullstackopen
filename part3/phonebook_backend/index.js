af126ee0dfc73d3c53b935c6820ed406a8b804db
realcomplex0 <mmkristins@gmail.com>
2024-01-09 14:24:06 +0200

    port numbers

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -85,7 +85,7 @@ app.post('/api/persons', (request, response) => {
     response.json(new_person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server listening on PORT ${PORT}`)
 })
