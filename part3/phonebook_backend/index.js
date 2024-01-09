625d2daed1a6922ac94b2f40d834f7ce48ff84db
realcomplex0 <mmkristins@gmail.com>
2024-01-09 15:15:04 +0200

    More deployment changes

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(cors({orgin : ["http://localhost:5173"]}))
 morgan.token('body', (request, response) => {
     return JSON.stringify(request.body)
